9d4cc652f865b4aa0aab1d04023fdf024527d405
Shivam Dixit <shivamd001@gmail.com>
2014-08-31 11:44:44 +0530

    Added initial stemmer
    Signed-off-by: Shivam Dixit <shivamd001@gmail.com>

---
+++ src/Stemmer.java
@@ -0,0 +1,38 @@
+class Stemmer
+{
+    public static boolean isVowel(String word, int i)
+    {
+        char c = Character.toLowerCase(word.charAt(i));
+        boolean flag = false;
+
+        switch (c) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                flag = true;
+                break;
+
+            case 'y':
+                if (i != 0) {
+                    if (!isVowel(word, i - 1)) {
+                        flag = true;
+                    }
+                }
+                break;
+        }
+
+        return flag;
+    }
+
+    // public static void getM(String word)
+    // {
+
+    // }
+
+    // public static void stem(String word)
+    // {
+
+    // }
+}
