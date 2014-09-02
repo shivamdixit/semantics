6ab34b46d90d214a6568e50cfbb85be425f3ff84
Shivam Dixit <shivamd001@gmail.com>
2014-09-02 12:57:14 +0530

    Added tests
    Signed-off-by: Shivam Dixit <shivamd001@gmail.com>

--- src/tests/TestStemmer.java
+++ src/tests/TestStemmer.java
@@ -9,6 +9,14 @@ public class TestStemmer
     @Test
     public void testIsVowel()
     {
-        //assertTrue(Stemmer.isVowel("shivam", 2));
+        assertFalse("'s' is not a vowel", Stemmer.isVowel("ShivamDixit", 0));
+        assertFalse("'h' is not a vowel", Stemmer.isVowel("ShivamDixit", 1));
+        assertFalse("'v' is not a vowel", Stemmer.isVowel("ShivamDixit", 3));
+        assertFalse("'y' is not a vowel", Stemmer.isVowel("Yamata", 0));
+        assertTrue("'a' is a vowel". Stemmer.isVowel("Yamata", 1));
+        assertTrue("'a' is a vowel", Stemmer.isVowel("ShivamDixit", 4));
+        assertTrue("'O' is a vowel", Stemmer.isVowel("Oliver", 0));
+        assertTrue(Stemmer.isVowel("Olyver", 2));
+        assertTrue(Stemmer.isVowel("HaPpY", 4));
     }
 }
