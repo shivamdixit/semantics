dff2d1ef5e100dd54ab973dcfd333c2da42ea161
Shivam Dixit <shivamd001@gmail.com>
2014-09-03 16:56:09 +0530

    Added tests for methods
    Signed-off-by: Shivam Dixit <shivamd001@gmail.com>

--- src/tests/TestStemmer.java
+++ src/tests/TestStemmer.java
@@ -1,11 +1,11 @@
 package tests;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 import semantics.*;
 
 public class TestStemmer
 {
-
     @Test
     public void testIsVowel()
     {
@@ -13,10 +13,50 @@ public class TestStemmer
         assertFalse("'h' is not a vowel", Stemmer.isVowel("ShivamDixit", 1));
         assertFalse("'v' is not a vowel", Stemmer.isVowel("ShivamDixit", 3));
         assertFalse("'y' is not a vowel", Stemmer.isVowel("Yamata", 0));
-        assertTrue("'a' is a vowel". Stemmer.isVowel("Yamata", 1));
+        assertTrue("'a' is a vowel", Stemmer.isVowel("Yamata", 1));
         assertTrue("'a' is a vowel", Stemmer.isVowel("ShivamDixit", 4));
         assertTrue("'O' is a vowel", Stemmer.isVowel("Oliver", 0));
         assertTrue(Stemmer.isVowel("Olyver", 2));
         assertTrue(Stemmer.isVowel("HaPpY", 4));
+    }
+
+    @Test
+    public void testGetM()
+    {
+        assertEquals(0, Stemmer.getM("TR"));
+        assertEquals(0, Stemmer.getM("EE"));
+        assertEquals(0, Stemmer.getM("TREE"));
+        assertEquals(0, Stemmer.getM("Y"));
+        assertEquals(0, Stemmer.getM("BY"));
+
+        assertEquals(1, Stemmer.getM("TROUBLE"));
+        assertEquals(1, Stemmer.getM("OATS"));
+        assertEquals(1, Stemmer.getM("TREES"));
+        assertEquals(1, Stemmer.getM("IVY"));
+
+        assertEquals(2, Stemmer.getM("Shivam"));
+        assertEquals(2, Stemmer.getM("PRIVATE"));
+        assertEquals(2, Stemmer.getM("ORRERY"));
+    }
+
+    @Test
+    public void testEnds()
+    {
+        assertFalse(Stemmer.ends("shivam", "a"));
+        assertFalse(Stemmer.ends("SHIVAM", "IV"));
+        assertTrue(Stemmer.ends("SHIVAM", "vam"));
+        assertTrue(Stemmer.ends("Quora", "A"));    // Case insensitive
+        assertTrue(Stemmer.ends("ShIvAM", "m"));   // Case insensitive
+        assertTrue(Stemmer.ends("a", "a"));        // Boundary case
+    }
+
+    @Test
+    public void testContainsVowel()
+    {
+        assertFalse(Stemmer.containsVowel("PHP"));
+        assertFalse(Stemmer.containsVowel("TWNDLLNGS"));
+        assertTrue(Stemmer.containsVowel("KVY"));
+        assertTrue(Stemmer.containsVowel("SuN"));
+        assertTrue(Stemmer.containsVowel("JOHN"));
     }
 }
